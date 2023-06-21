cb2b090744d869c3cca466f68e62c30279851a53
Sameera Premathunga <premathunga@live.com>
2023-06-21 11:11:34 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import OneSignal from 'react-native-onesignal';
 function MyComponent() {
   useEffect(() => {
     OneSignal.setLogLevel(6, 0);
-    OneSignal.setAppId('16bc18f4-d0c0-4734-9423-a4dab550dd47');
+    OneSignal.setAppId('16bc18f4-d0c0-4734-9423-a4dab550xx47');
     OneSignal.setRequiresUserPrivacyConsent(false);  
     OneSignal.promptForPushNotificationsWithUserResponse((response) => {   
       console.log('Prompt response:', response);   
